8bb6fffbb66a504d45109c5f4779b9e5bafaedc7
Josh Hall <joshuadeanhall@gmail.com>
2015-01-29 14:16:05 -0500

    Initial commit

---
+++ GitHubBackup/GitHubBackup/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GitHubBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(12, 13);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(100, 20);
+            this.tbUserName.TabIndex = 1;
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Location = new System.Drawing.Point(12, 84);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.ReadOnly = true;
+            this.tbStatus.Size = new System.Drawing.Size(260, 166);
+            this.tbStatus.TabIndex = 3;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 55);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Backup";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.tbUserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ GitHubBackup/GitHubBackup/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibGit2Sharp;
+using Octokit;
+using Repository = LibGit2Sharp.Repository;
+
+namespace GitHubBackup
+{
+    public partial class Form1 : Form
+    {
+        private string _folderLocation;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            _folderLocation = folderBrowserDialog1.SelectedPath;
+        }
+
+        private async void btnRun_Click(object sender, EventArgs e)
+        {
+            var userName = tbUserName.Text;
+            if (string.IsNullOrEmpty(userName))
+                return;
+            if (Directory.Exists("repos") == false)
+            {
+                Directory.CreateDirectory("repos");
+            }
+            var github = new GitHubClient(new ProductHeaderValue("GitHubBackTester"));
+            var repositories = await github.Repository.GetAllForUser(userName);
+         
+            foreach (var githubRepository in repositories)
+            {
+                var path = Path.Combine("repos", githubRepository.Name);
+                Repository.Clone(githubRepository.HtmlUrl, path);
+            }
+        }
+    }
+}
